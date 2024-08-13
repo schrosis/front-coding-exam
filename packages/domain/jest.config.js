4f59db72f724d62d967c8f1c19818bbbd895f9ce
schrosis <54675948+schrosis@users.noreply.github.com>
2024-08-13 20:48:03 +0900

    chore: パッケージ名にプレフィックス追加

--- packages/domain/jest.config.js
+++ packages/domain/jest.config.js
@@ -1,4 +1,4 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  preset: "./node_modules/dev-tools/jest.config.js",
+  preset: "./node_modules/@front-coding-exam/dev-tools/jest.config.js",
 };
