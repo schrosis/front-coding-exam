bd5885e6f6f8a1306f91807eaa656b086fb18f5c
schrosis <54675948+schrosis@users.noreply.github.com>
2024-08-14 22:53:47 +0900

    chore: 人口構成APIのモック作成

--- packages/infra/src/libs/msw/resas.ts
+++ packages/infra/src/libs/msw/resas.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from "msw";
 import { baseURL } from "../../resas";
+import type { PopulationCompositionsApiResponse } from "../../resas/endpoints/populationCompositions";
 import type { PrefecturesApiResponse } from "../../resas/endpoints/prefectures";
 import type { ErrorResponse } from "../../resas/error";
 
@@ -15,4 +16,99 @@ export const getPrefecturesApi = (errorResponse?: ErrorResponse) => {
 
     return HttpResponse.json(errorResponse ?? response, { status: 200 });
   });
+};
+
+export const getPopulationCompositionsApi = (errorResponse?: ErrorResponse) => {
+  return http.get(
+    new URL("api/v1/population/composition/perYear", baseURL).toString(),
+    () => {
+      const response: PopulationCompositionsApiResponse = {
+        message: null,
+        result: {
+          boundaryYear: 2020,
+          data: [
+            {
+              label: "総人口",
+              data: [
+                {
+                  year: 2010,
+                  value: 10888,
+                },
+                {
+                  year: 2015,
+                  value: 10133,
+                },
+                {
+                  year: 2020,
+                  value: 9302,
+                },
+              ],
+            },
+            {
+              label: "年少人口",
+              data: [
+                {
+                  year: 2010,
+                  value: 1321,
+                  rate: 12.13,
+                },
+                {
+                  year: 2015,
+                  value: 1144,
+                  rate: 11.29,
+                },
+                {
+                  year: 2020,
+                  value: 936,
+                  rate: 10.06,
+                },
+              ],
+            },
+            {
+              label: "生産年齢人口",
+              data: [
+                {
+                  year: 2010,
+                  value: 6387,
+                  rate: 58.66,
+                },
+                {
+                  year: 2015,
+                  value: 5538,
+                  rate: 54.65,
+                },
+                {
+                  year: 2020,
+                  value: 4756,
+                  rate: 51.13,
+                },
+              ],
+            },
+            {
+              label: "老年人口",
+              data: [
+                {
+                  year: 2010,
+                  value: 3179,
+                  rate: 29.2,
+                },
+                {
+                  year: 2015,
+                  value: 3442,
+                  rate: 33.97,
+                },
+                {
+                  year: 2020,
+                  value: 3578,
+                  rate: 38.46,
+                },
+              ],
+            },
+          ],
+        },
+      };
+
+      return HttpResponse.json(errorResponse ?? response, { status: 200 });
+    },
+  );
 };
