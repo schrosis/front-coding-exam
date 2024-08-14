f35ceb33a236ac2cd160086971850018700d0d55
schrosis <54675948+schrosis@users.noreply.github.com>
2024-08-14 22:53:24 +0900

    refactor: 型修正 ResasApiClient

--- packages/infra/src/resas/client.ts
+++ packages/infra/src/resas/client.ts
@@ -1,15 +1,35 @@
+import type { AxiosInstance } from "axios";
 import { baseURL } from ".";
 import { createClient } from "../libs/axios";
+import * as populationCompositions from "./endpoints/populationCompositions";
 import * as prefectures from "./endpoints/prefectures";
 
-export type ResasApiClient = {
-  getPrefectures(): ReturnType<typeof prefectures.getPrefectures>;
+type EndpointFunction<Args extends unknown[], Return> = (
+  client: AxiosInstance,
+  ...args: Args
+) => Return;
+
+type InferClient<
+  T extends {
+    [K: string]: EndpointFunction<never, unknown>;
+  },
+> = {
+  [K in keyof T]: T[K] extends EndpointFunction<infer Args, infer Return>
+    ? (...args: Args) => Return
+    : never;
 };
+
+export type ResasApiClient = InferClient<{
+  getPrefectures: typeof prefectures.getPrefectures;
+  getPopulationCompositions: typeof populationCompositions.getPopulationCompositions;
+}>;
 
 export const createResasApiClient = (apiKey: string): ResasApiClient => {
   const client = createClient(baseURL, apiKey);
 
   return {
-    getPrefectures: () => prefectures.getPrefectures(client),
+    getPrefectures: (...args) => prefectures.getPrefectures(client, ...args),
+    getPopulationCompositions: (...args) =>
+      populationCompositions.getPopulationCompositions(client, ...args),
   };
 };
