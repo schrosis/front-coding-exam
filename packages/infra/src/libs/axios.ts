96ac0fe6d417c8a019820cadc59e93c16a42b1c1
schrosis <54675948+schrosis@users.noreply.github.com>
2024-08-18 15:48:02 +0900

    chore: axios_rate_limit 導入

--- packages/infra/src/libs/axios.ts
+++ packages/infra/src/libs/axios.ts
@@ -1,9 +1,16 @@
 import Axios, { type AxiosInstance } from "axios";
+import rateLimit from "axios-rate-limit";
 
-export const createClient = (baseURL: string, apiKey: string): AxiosInstance =>
-  Axios.create({
+export const createClient = (
+  baseURL: string,
+  apiKey: string,
+): AxiosInstance => {
+  const instance = Axios.create({
     baseURL: baseURL,
     headers: {
       "X-API-KEY": apiKey,
     },
   });
+
+  return rateLimit(instance, { maxRequests: 5, perMilliseconds: 1000 });
+};
