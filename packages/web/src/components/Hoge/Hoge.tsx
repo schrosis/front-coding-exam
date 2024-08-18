4c61a2ea75d140349790e8f5c4f9b091c6396708
schrosis <54675948+schrosis@users.noreply.github.com>
2024-08-18 17:52:21 +0900

    chore: vanilla-extract 導入

--- packages/web/src/components/Hoge/Hoge.tsx
+++ packages/web/src/components/Hoge/Hoge.tsx
@@ -1,9 +1,10 @@
 import type { FC } from "react";
+import { className } from "./Hoge.css";
 
 type Props = {
   text: string;
 };
 
 export const Hoge: FC<Props> = ({ text }) => {
-  return <div>{text}</div>;
+  return <div className={className}>{text}</div>;
 };
